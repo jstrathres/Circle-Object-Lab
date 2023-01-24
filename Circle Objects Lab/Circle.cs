a0b3df6f064d2ca461a79c9a934a1a58d2a0611c
jstrathres <jstraz7@gmail.com>
2023-01-23 20:15:57 -0500

    first commit

---
+++ Circle Objects Lab/Circle.cs
@@ -0,0 +1,51 @@
+﻿using Circle_Objects_Lab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Objects_Lab
+{
+    public class Circle
+    {
+        //properties
+        private double radius;
+
+        public double Radius
+        {
+            get { return radius; }
+        }
+
+        //C = πd 
+        //Constructor
+        public Circle(double radius)
+        {
+            this.radius = radius; //the single entered input
+        }
+
+        //Method
+        public double CalculateCircumference() //returns the basic Circumference
+        {
+            return 2 * Math.PI * radius;
+        }
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference());
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);//basic math for 
+        }
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+        private string FormatNumber(double x)
+        {
+            return x.ToString("0.00");
+        }
+    }
+}
+
+
---
+++ Circle Objects Lab/Program.cs
@@ -0,0 +1,35 @@
+﻿using Circle_Objects_Lab;
+Console.WriteLine("Welcome to the Circle Tester");
+
+int circleCount = 0;
+bool restart = true;
+
+while (restart)
+{
+    Console.WriteLine("Enter radius:");
+    double radius;
+    while(!double.TryParse(Console.ReadLine(), out radius))
+    {
+        Console.WriteLine("Invalid input: Try again");
+    }
+
+    Circle circle = new Circle(radius); //dont quite understand this
+    Console.WriteLine("Circumference" + circle.CalculateFormattedCircumference());
+    Console.WriteLine("Area" +circle.CalculateFormattedArea());
+    circleCount++;
+
+    Console.WriteLine("Continue? y/n");
+    string response = Console.ReadLine();
+
+    if(response == "y")
+    {
+        restart = true;
+    }
+    else
+    {
+        Console.WriteLine("Goodbye. You created " + circleCount + " Circle Objects");
+        restart= false;
+    }    
+}
+
+
